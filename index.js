84e9d7abf37aa9694f79b28d073442ed23e8e05d
sej197 <141762007+sej197@users.noreply.github.com>
2025-01-11 09:39:30 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor((randomNumber1 * 6) + 1);
-var file1 = "image/dice" + randomNumber1 + ".png";
-var image1 = document.querySelector("img")[0];
-image1.setAttribute("src", file1 );
